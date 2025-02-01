a69d5c7ecd3ec852edd847c4c6efeb062be47432
Juan Ramallo <ignaramallo@Juans-Mac-mini.local>
2025-02-01 17:58:06 -0300

    fix: env file

--- src/modules/firebase/firebase.module.ts
+++ src/modules/firebase/firebase.module.ts
@@ -2,10 +2,6 @@
 import { Module } from '@nestjs/common';
 import * as admin from 'firebase-admin';
 import { FirebaseService } from './firebase.service';
-import serviceAccount from './sodastream-app-firebase-adminsdk-d8c2k-f419fc9b0e.json';
-import { ServiceAccount } from 'firebase-admin';
-
-const serviceAccountTyped = serviceAccount as ServiceAccount;
 
 @Module({
   providers: [FirebaseService],
@@ -15,8 +11,11 @@ export class FirebaseModule {
   constructor() {
     // Inicializar Firebase (credenciales del archivo JSON)
     admin.initializeApp({
-      credential: admin.credential.cert(serviceAccountTyped),
-      // databaseURL: 'https://<your-database-name>.firebaseio.com' (opcional si usas Firestore)
+      credential: admin.credential.cert({
+        projectId: process.env.PROJECT_ID,
+        clientEmail: process.env.CLIENT_EMAIL,
+        privateKey: process.env.PRIVATE_KEY,
+      }),
     });
   }
 }
